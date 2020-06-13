303f83a5cc53f42f9dea1647836bd188f3896b02
Nonthasut <nonthasut.phanphon@gmail.com>
2020-06-13 21:22:38 +0700

    Finish

--- homework-js/javascriptBasicQuiz.js
+++ homework-js/javascriptBasicQuiz.js
@@ -37,15 +37,82 @@ null + 1 //1
 undefined + 1 //NaN
 " \t \n" - 2 //NaN
 //Slide 132-133
-6.5: Yes
 
 let question = prompt("Who am I?");
-let ans;
-ans=(question=="Pack")? "You're awesome.":"You don't know you are.";
+let ans=(question=="Pack")? "You're awesome.":"You don't know you are.";
 alert(ans);
 
-
-//Slide134
 let input = prompt("How much your score?");
-let ans = (input >)
+let ans;
+if(input >= 80){
+    alert(ans = "A");
+}
+else if(input >= 70){
+    alert(ans = "B");
+}
+else if(input >= 60){
+    alert(ans = "C");
+}
+else if(input >= 50){
+    alert(ans = "D");
+}
+else{
+    alert(ans = "F");
+}
+
+let age = prompt("How old are you?");
+let price;
+let ans = (age <= 18)? price=2000:price=3500;
+alert(price)
+//ไม่ได้แก้บัคถ้าพิมพ์ประเภทข้อมูลคือนอกจาก Number เข้าไป
+
+// 1. จงคำนวณค่าด้านล่างต่อไปนี้ และ คอมเม้นตอบหลังคำถาม
+5 + "34"            // 534
+5 ** 4              // 625
+5 - "4"             // 1
+10 % 5              //0
+5 % 10;             //5
+(2 ** 5) % 2        //0
+"Java" + "Script"   //JavaScript    
+" " + " "           //
+" " + 0             //0
+20 + 4 + "15"       //2415
+true + true         //2
+true + false        //1
+false + true        //1
+false - true        //-1
+false - true + false - true + false;    //-2
+(true + true) ** 4  //16
+19 + true + "20"    //2020
+3 - 4               //-1
+"Bob" - "bill"      //NaN
+"Code" + "Camp" + true + false + null + undefined //CodeCamptruefalsenullundefined
+
+
+// 2. จงคำนวณค่าเปรียบเทียบต่อไปนี้ และ คอมเม้นตอบด้านหลังเหมือนข้างบน
+5 >= 1  //true
+0 === 1 //false
+4 <= 1  //false
+1 != 1  //false
+1 == "1"    //true
+1 === "1"   //false
+"2" == "2"  //true
+"2" === "2" //true
+"12" !== 12 //true
+"15" != 15  //false
+"A" > "B"   //false
+"B" < "C"   //true
+"a" > "A"   //true
+"b" < "A"   //false
+"bee" > "bbe"   //true
+"BeE" < "Bee"   //true
+"SonTer" > "Soncom" //false
+"circle" > "circles"    //false
+"NaRuTo" < "nArUtO" //true
+true === false //false
+true != true    //false
+
+
+// 3. สร้าง String ที่มีค่า "Hello, It's me. "Mario"."
+let str = '"Hello, It\'s me. "\Mario".\"'
 
