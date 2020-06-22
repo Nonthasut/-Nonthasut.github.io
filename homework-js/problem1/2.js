b8ade39238393d2315c31cff23e2b3cf42c444a6
Nonthasut <nonthasut.phanphon@gmail.com>
2020-06-22 23:13:43 +0700

    renovateFile

--- homework-js/problem1/2.js
+++
@@ -1,8 +0,0 @@
-let sum = 0;
-for (i = 160; i <= 2048; i++) {
-  if (i % 2 == 0) {
-    sum += i
-  } else if (i % 2 != 0) {
-    sum += 0;
-  }
-}
--- toDoList/toDoList.js
+++
@@ -1,7 +0,0 @@
-function getList() {
-   let textList = document.getElementById("inputAc").Value;
-   let addText = document.createTextNode(textList);
-   let list = document.createElement("li");
-   list.appendChild(addText);
-   document.getElementById(lis).appendChild(list);
-}
